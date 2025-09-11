7ebc8441845ea8c0133f306f5d27431f10b9aaec
Awais Malik <muhamadawaismalik@gmail.com>
2025-09-11 23:16:43 +0500

    Updated MYSQL Script

--- mysql/script.sh
+++ mysql/script.sh
@@ -5,9 +5,12 @@ mysqld_safe &
 
 sleep 10
 
+DBUSER="wordpress"
+DBPASS="awaismalik"
+
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
-mysql -u root -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED BY 'awaismalik';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$DBUSER'@'%' IDENTIFIED BY '$DBPASS';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$DBUSER'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root shutdown
